0d77d8033edb8bd043697986d17ef75c200273f0
user <kozyan94@gmail.com>
2022-09-28 00:11:54 +0300

    hw 2.4 Exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,17 @@
 public class Main {
-    public static boolean loginCheck(String login, String password, String confirmPassword) {
-        boolean loginSign = login.matches("&93hjffsdf$");
-        boolean passwordSign = login.matches("&93h^^jfd_sfsd/f$");
-        boolean passwordCompare = confirmPassword.equals(password);
 
-        if (!loginSign || login.length() > 20) {
-            try {
-                throw new WrongLoginException();
-            } catch (WrongLoginException exception) {
-                System.out.println("Login должен содержать в себе только латинские буквы, цифры и знак подчеркивания и быть не больше 20 символов");
-            }
-        } else if (!passwordSign || password.length() >= 20) {
-            try {
-                throw new WrongLoginException();
-            } catch (WrongLoginException exception) {
-                System.out.println("Password должен содержать в себе только латинские буквы, цифры и знак подчеркивания и быть меньше 20 символов");
-            }
-        } else if (!passwordCompare) {
-            try {
-                throw new WrongLoginException();
-            } catch (WrongLoginException exception) {
-                System.out.println("password и confirmPassword должны быть равны");
-            }
-        } else System.out.println("Пользователь зарегистрирован");
-        return true;
+    public static void printResult(String login, String password, String confirmPassword) {
+        InputData verification = new InputData(login, password, confirmPassword);
+        System.out.println("\r\n" + "Login: " + login + ", " +
+                "Password: " + password + ", " +
+                "Confirm: " + confirmPassword);
+        boolean result = verification.checkLoginParameters();
+        System.out.println("result = " + result);
     }
 
 
     public static void main(String[] args) {
-        String login = "java_skypro.go";
-        String password = "D_1hWiKjjP_9";
-        String confirmPassword = "D_1hWiKjjP_9";
-        loginCheck(login, password, confirmPassword);
+        printResult("java_skypro.go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
+        printResult("java_skyprogo", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
     }
 }
